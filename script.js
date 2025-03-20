49b475889617467acdcc07ac8847474ee4cabdad
Daniel <wangdaniel913@gmail.com>
2025-03-20 11:25:48 -0400

    Stuff
    Yeah

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let startButton2 = document.querySelector(".startButton2");
 let startButton3 = document.querySelector(".startButton3");
 let restart = document.querySelector(".restart");
 let page = document.querySelector(".fixed");
+let title = document.querySelector(".title");
 
 restart.addEventListener("click", function () {
   location.reload();
@@ -12,10 +13,14 @@ restart.addEventListener("click", function () {
 //Start page script
 startButton1.addEventListener("click", function () {
   start.style.display = "none";
+  title.innerHTML = "Where Would You Like To Go?";
 });
 startButton2.addEventListener("click", function () {
   start.style.display = "none";
+  title.innerHTML = "Where Would You Like To Go?";
 });
 startButton3.addEventListener("click", function () {
   start.style.display = "none";
+  title.innerHTML = "Where Would You Like To Go?";
 });
+//Japan
