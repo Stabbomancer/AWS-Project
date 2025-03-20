d102e8efd9aaf4acab069557fec8570ef54e78fc
Stabbomancer <samuelgeltman1@gmail.com>
2025-03-20 12:44:30 -0400

    Co-authored-by: Pedrov2717 <Pedrov2717@users.noreply.github.com> Co-authored-by: DanielW114 <DanielW114@users.noreply.github.com> Co-authored-by: Azmaine <Azmaine@users.noreply.github.com> Co-authored-by: Grsablaze <Grsablaze@users.noreply.github.com> Co-authored-by: Delta <Delta-Saber@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ let startButton1 = document.querySelector(".startButton1");
 let startButton2 = document.querySelector(".startButton2");
 let startButton3 = document.querySelector(".startButton3");
 let restart = document.querySelector(".restart");
-let page = document.querySelector(".fixed");
 let title = document.querySelector(".title");
+let japanStart = document.querySelector(".japanBox1");
+let jpOption1 = document.querySelector(".jp1");
 
 restart.addEventListener("click", function () {
   location.reload();
@@ -20,7 +21,11 @@ startButton2.addEventListener("click", function () {
   title.innerHTML = "Where Would You Like To Go?";
 });
 startButton3.addEventListener("click", function () {
-  start.style.display = "none";
   title.innerHTML = "Where Would You Like To Go?";
+  start.style.display = "none";
+  japanStart.style.display = "flex";
 });
 //Japan
+jpOption1.addEventListener("click", function () {
+  japanStart.style.display = "none";
+});
